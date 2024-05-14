b5cbfc8b3733ca59f6c2c8d15f3b939a0d9d9dfb
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2024-05-14 16:14:00 +0530

    table rows listing

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,45 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box } from '@mui/material';
 
 const FeedBackTable = () => {
-  const [submissions, setSubmissions] = useState([]);
+  const [submissions, setSubmissions] = useState(JSON.parse(localStorage.getItem('feedbackData')) || []);
+
+ 
 
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('formData'));
+    const data = JSON.parse(localStorage.getItem('feedbackData'));
     if (data) {
-      setSubmissions([data]);
+      setSubmissions(data);
     }
   }, []);
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Customer Name</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Phone</TableCell>
-            <TableCell>Service Quality</TableCell>
-            <TableCell>Beverage Quality</TableCell>
-            <TableCell>Cleanliness</TableCell>
-            <TableCell>Overall Experience</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {submissions.map((submission, index) => (
-            <TableRow key={index}>
-              <TableCell>{submission.customerName}</TableCell>
-              <TableCell>{submission.email}</TableCell>
-              <TableCell>{submission.phone}</TableCell>
-              <TableCell>{submission.serviceQuality}</TableCell>
-              <TableCell>{submission.beverageQuality}</TableCell>
-              <TableCell>{submission.cleanliness}</TableCell>
-              <TableCell>{submission.overallExperience}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box 
+        sx={{
+            border: '1px solid red',
+            width: '95%',
+            margin: 'auto'
+        }}
+    >
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Phone</TableCell>
+                        <TableCell>Service Quality</TableCell>
+                        <TableCell>Beverage Quality</TableCell>
+                        <TableCell>Cleanliness</TableCell>
+                        <TableCell>Overall Exp.</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {submissions.map((submission, index) => (
+                        <TableRow key={index}>
+                            <TableCell>{submission.customerName}</TableCell>
+                            <TableCell>{submission.email}</TableCell>
+                            <TableCell>{submission.phone}</TableCell>
+                            <TableCell>{submission.serviceQuality}</TableCell>
+                            <TableCell>{submission.beverageQuality}</TableCell>
+                            <TableCell>{submission.cleanliness}</TableCell>
+                            <TableCell>{submission.overallExperience}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Box>
   );
 };
 
